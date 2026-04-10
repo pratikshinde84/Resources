fa86097f0b2ddac4744386ecdc19bf4e94c22ccc
Pratik <pratik216029@gmail.com>
2026-04-11 01:41:54 +0530

    done

--- DAA/Solution.java
+++ DAA/Solution.java
@@ -21,7 +21,7 @@ class Solution {
         }
         return ar;
     }    
-    public static void main(String[] args) { 
+    public static void main(String[] args) {  
         System.out.print(nextLargerElement(new int[]{6,8,0,1,3}));
     }
 }
