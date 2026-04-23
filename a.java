059cb2de119d0f1a6c28a55b469c17e0b2dea06c
Pratik <pratik216029@gmail.com>
2026-04-23 17:06:56 +0530

    sds

--- a.java
+++ a.java
@@ -3,17 +3,12 @@ class a{
         StringBuilder sb=new StringBuilder(s);
         int i=0;
         while(i<sb.length()-1){
-            if(sb.charAt(i)>96&&sb.charAt(i)<123){
-                if(sb.charAt(i+1)==sb.charAt(i)+32){
-                    sb.deleteCharAt(i);
-                    sb.deleteCharAt(i);
-                    if(sb.isEmpty()){
-                        return "";
-                    }
-                    i=0;
-                }
+            if(Math.abs(sb.charAt(i)-sb.charAt(i+1))==32){
+                sb.delete(i,i+2);
+                i=Math.max(0,i-1);
+            }else{
+                i++;
             }
-            i++;
         }
         return sb.toString();
     }
