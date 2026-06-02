e0d3248df17c3ef9f0a5fcb02de82846e9330a42
Pratik <pratik216029@gmail.com>
2026-06-02 11:32:45 +0530

    done

--- Min_Heap.java
+++ Min_Heap.java
@@ -19,7 +19,7 @@ public class Min_Heap {
     public static void main(String[] args) {  
         Min_Heap mh=new Min_Heap(); 
         int arr[]={1,2,3}; 
-        int n=arr.length;  
+        int n=arr.length;
         System.out.println(mh.isMinHeap(arr, n));
     }
 }
